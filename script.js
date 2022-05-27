f3fffc4c1e7f5bed23e02bf8be80ca26fdbf07bc
Gabriel Moreira Mourão Dai <Gabriel.Dai@notacontrol.matriz>
2022-05-27 16:49:46 -0400

    criando divs filhas

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 var id = 0;
+var minhasDivs = []
+var corpo = document.getElementById('corpo');
 
-function criaDiv(btn = 0) {
+function criaDiv(btn = undefined) {
     //criando o elemento
     var div = document.createElement('div');
     div.id = 'txt'+id;
     div.className = 'box';
-    div.innerHTML = '<textarea placeholder="seu texto aqui"></textarea><button style="border:none; border-radius:50%" onclick="criaDiv(1);">+</button>';
+    div.innerHTML = '<textarea placeholder="seu texto aqui"></textarea><button style="border:none; border-radius:50%" onclick="criaDiv(this);">+</button>';
     document.body.appendChild(div);
 
     var x, y;
     //definindo sua posição
-    if (btn == 0) {
+    if (btn === undefined) {
         x = event.clientX+'px';
         y = event.clientY+'px';
     }
     else {
-        let txtId = 'txt'+id-1;
-        var xX = document.getElementById(txtId)
-        x = xX.style.top.x;
-        y = xX.style.top.y + 50;
+        let idNovo = btn.parentNode.id;
+        var xX = document.getElementById(idNovo);
+        x = xX.style.left.replace('px', '');
+        x = parseInt(x) + 300 + 'px';
+        y = xX.style.top;
     }
     console.log(x, y);
     div.style.position = 'absolute';
@@ -26,6 +29,8 @@ function criaDiv(btn = 0) {
     div.style.left = x;
 
     id++;
+
+    minhasDivs.push(div);
 }
 
 
