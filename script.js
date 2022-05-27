899960425d5a8e1ce89acf4cb2780c05ca87e11a
Gabriel Moreira Mourão Dai <Gabriel.Dai@notacontrol.matriz>
2022-05-27 15:58:08 -0400

    projeto iniciado/add div ao clicar

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-function criaDiv() {
+var id = 0;
+
+function criaDiv(btn = 0) {
     //criando o elemento
     var div = document.createElement('div');
+    div.id = 'txt'+id;
     div.className = 'box';
-    div.innerHTML = '<input placeholder="seu texto aqui"/>'
+    div.innerHTML = '<textarea placeholder="seu texto aqui"></textarea><button style="border:none; border-radius:50%" onclick="criaDiv(1);">+</button>';
     document.body.appendChild(div);
 
+    var x, y;
     //definindo sua posição
-    var x = event.clientX+'px';
-    var y = event.clientY+'px';
+    if (btn == 0) {
+        x = event.clientX+'px';
+        y = event.clientY+'px';
+    }
+    else {
+        let txtId = 'txt'+id-1;
+        var xX = document.getElementById(txtId)
+        x = xX.style.top.x;
+        y = xX.style.top.y + 50;
+    }
     console.log(x, y);
     div.style.position = 'absolute';
     div.style.top = y;
     div.style.left = x;
+
+    id++;
 }
+
+
